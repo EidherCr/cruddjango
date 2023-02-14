e038b978a3f25d34791e4807c524ec29848a8939
EidherCr <71907463+EidherCr@users.noreply.github.com>
2023-02-13 21:14:55 -0400

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-  title = models.CharField(max_length=200)
-  description = models.TextField(max_length=1000)
+  titulo = models.CharField(max_length=200)
+  descripcion = models.TextField(max_length=1000)
   created = models.DateTimeField(auto_now_add=True)
   datecompleted = models.DateTimeField(null=True, blank=True)
-  important = models.BooleanField(default=False)
+  importante = models.BooleanField(default=False)
   user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
