927eeae36a00f2282958ac69213a5f34fd3baf95
EidherCr <71907463+EidherCr@users.noreply.github.com>
2023-02-20 20:49:23 -0400

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -56,8 +56,8 @@ def signup(request):
 
         return render(request, 'signup.html', {"form": UserCreationForm, "error": "Passwords did not match."})
     
- def create_mail(user_mail, subject, template_name, context):
-    template = get_template(template_name)
+ def create_mail(user_mail, subject, 'signup.html', context):
+    template = get_template('signup.html')
     content = template.render(context)
 
     message = EmailMultiAlternatives(
